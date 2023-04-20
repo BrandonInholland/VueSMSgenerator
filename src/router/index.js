033dc8e87a62d72ed03a070ca5ba7e84ae1b5552
BrandonInholland <42965932+BrandonInholland@users.noreply.github.com>
2023-04-20 12:35:44 +0200

    clean up

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '../components/Home.vue';
-import lecturerEmail from '../components/MessageWithLecturerMail.vue';
-import MessageItem from '../components/MessageItem.vue';
+
 import GetAllClasses from '../components/GetAllClasses.vue';
-import AddClasses from '../components/AddClasses.vue';
-import AddLecturer from '../components/AddLecturer.vue';
+
 import GetAllLecturers from '../components/GetAllLecturers.vue';
-import AddStudent from '../components/AddStudent.vue';
+
 import GetStudent from '../components/GetStudentById.vue';
-import DeleteStudent from '../components/StudentSoftDelete.vue';
+
 import FrontPage from '../components/FrontPage.vue';
 import sendMessage from '../components/SendMessage.vue';
 
@@ -18,15 +16,13 @@ import sendMessage from '../components/SendMessage.vue';
 const routes = [
   { path: '/', component: Home },
   { path: '/frontpage', component: FrontPage },
-  { path: '/getbymessageid', component: MessageItem },
-  { path: '/messageWithlecturermail', component: lecturerEmail },
+  
   { path: '/getallclasses', component: GetAllClasses },
-  { path: '/addclasses', component: AddClasses },  
-  { path: '/AddLecturer', component: AddLecturer },
+  
   { path: '/getalllecturers', component: GetAllLecturers },
-  { path: '/addstudent', component: AddStudent }, 
+  
   { path: '/getstudent', component: GetStudent }, 
-  { path: '/deletestudent', component: DeleteStudent }, 
+  
   { path: '/sendMessage', component: sendMessage },
 ];
 
